61322921cdf6d621d3377bcee5acbdb7b4f01e77
iMookatayou <jetsribumrungtechin@gmail.com>
2026-07-14 10:07:03 +0700

    feat/exercise3: add typed Home and UsersList pages with routing

--- src/pages/UserList.tsx
+++ src/pages/UserList.tsx
@@ -1,14 +1,18 @@
 import { Link } from "react-router-dom";
 
 // TODO2: กำหนด Type สำหรับ user
+type User = {
+  id: number;
+  name: string
+}
 
 const UsersList = () => {
-  const users = [
+  const users: User[] = [
     { id: 1, name: "Alice" },
     { id: 2, name: "Bob" },
     { id: 3, name: "Charlie" },
   ];
-
+  
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <h1 className="text-3xl font-bold mb-6">Users List</h1>
